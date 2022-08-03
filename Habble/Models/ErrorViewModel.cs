7a38542e756e14455fb118207503504eadebef1b
Gilbert Mule <manstill09@outlook.com>
2022-08-03 19:15:37 +0300

    Add project files.

---
+++ Habble/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Habble.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
